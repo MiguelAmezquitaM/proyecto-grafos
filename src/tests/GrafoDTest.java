6769a40ca16e066c31bd4aa47fb99766d8413881
MiguelAmezquitaM <maamezquita@unimagdalena.edu.co>
2022-11-17 14:37:40 -0500

    testing

--- src/tests/GrafoDTest.java
+++ src/tests/GrafoDTest.java
@@ -10,7 +10,7 @@ import grafo.GrafoD;
 public class GrafoDTest {
     @Test
     public void addVertice() {
-        Grafo<String> g = new GrafoD<>();
+        Grafo<String, Double> g = new GrafoD<>();
         g.addVertice("8");
         g.addVertice("9");
         g.addVertice("2");
@@ -23,18 +23,18 @@ public class GrafoDTest {
 
     @Test
     public void addArista() {
-        Grafo<String> g = new GrafoD<>();
+        Grafo<String, Double> g = new GrafoD<>();
         g.addVertice("8");
         g.addVertice("9");
         g.addVertice("2");
-        g.addCosto(0, 1, 152);
-        g.addCosto(1, 2, 485);
-        g.addCosto(2, 0, 740);
+        g.addCosto(0, 1, 152.0);
+        g.addCosto(1, 2, 485.0);
+        g.addCosto(2, 0, 740.0);
         assertEquals(152, g.getCosto(0, 1), 0.1);
         assertEquals(485, g.getCosto(1, 2), 0.1);
         assertEquals(740, g.getCosto(2, 0), 0.1);
-        assertEquals(Grafo.INF, g.getCosto(0, 2), 0.1);
-        assertEquals(Grafo.INF, g.getCosto(1, 0), 0.1);
-        assertEquals(Grafo.INF, g.getCosto(2, 1), 0.1);
+        assertEquals(null, (Object) g.getCosto(0, 2));
+        assertEquals(null, (Object) g.getCosto(1, 0));
+        assertEquals(null, (Object) g.getCosto(2, 1));
     }
 }
