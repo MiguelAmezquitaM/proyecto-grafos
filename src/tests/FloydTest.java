3f3e1fdced020e33e521d2d89cc8956bb8f7e5a9
MiguelAmezquitaM <maamezquita@unimagdalena.edu.co>
2022-11-25 00:17:35 -0500

    testing floyd algorithm

--- src/tests/FloydTest.java
+++ src/tests/FloydTest.java
@@ -44,24 +44,34 @@ public class FloydTest {
         g.addVertice("9");
         g.addVertice("2");
         g.addVertice("15");
-        g.addCosto(0, 1, 152.0);
+        g.addVertice("58");
+        g.addCosto(0, 1, 41.1);
         g.addCosto(0, 2, 252.0);
         g.addCosto(0, 3, 452.0);
+        g.addCosto(0, 4, 885.2);
         g.addCosto(1, 0, 45.0);
         g.addCosto(1, 2, 100.0);
+        g.addCosto(1, 4, 17.0);
         g.addCosto(2, 0, 540.0);
         g.addCosto(2, 1, 620.0);
+        g.addCosto(2, 4, 94.0);
         g.addCosto(2, 3, 25.0);
         g.addCosto(3, 0, 640.0);
-        g.addCosto(3, 1, 240.0);
+        g.addCosto(3, 2, 60.0);
+        g.addCosto(3, 4, 21.8);
+        g.addCosto(4, 0, 40.0);
+        g.addCosto(4, 2, 20.0);
 
         floyd = new Floyd<>(g, new DoubleIndicator(), Double.class);
         var r = floyd.getRecorridos();
-        assertEquals(2, r[0][3]);
+        assertEquals(4, r[0][3]);
         assertEquals(3, r[2][3]);
         assertEquals(0, r[1][0]);
-        assertEquals(1, r[3][1]);
-        assertEquals(1, r[3][0]);
-        assertEquals(3, r[2][1]);
+        assertEquals(4, r[3][1]);
+        assertEquals(4, r[3][0]);
+        assertEquals(4, r[2][1]);
+        assertEquals(0, r[4][0]);
+        assertEquals(2, r[4][3]);
+        assertEquals(1, r[0][4]);
     }
 }
